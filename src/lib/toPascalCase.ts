3c34cf48713c4a83e53b404dc00a0c6eeddb72dd
David AfterDark <hanktech1.0@gmail.com>
2024-10-31 07:10:39 -0400

    feat: create toPascalCase

---
+++ src/lib/toPascalCase.ts
@@ -0,0 +1,13 @@
+export function toPascalCase(text: string) {
+    const splitText = text.split('-') // Divide el texto en palabras usando el guion como delimitador
+    //console.log('splitText: ', splitText)
+
+    
+    const capitalizeText = splitText.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Mayúscula + resto en minúsculas
+    //console.log('capitalizeText: ', capitalizeText)
+    
+    const pascalCaseText = capitalizeText.join('')
+    //console.log(pascalCaseText)
+
+    return pascalCaseText
+}
